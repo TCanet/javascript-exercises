a9e1d0a92c1e5001062884defc42dc6d7ac47cba
Titus Canet <tcanet94@gmail.com>
2022-11-28 13:26:57 -0800

    Added function logic to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) 
+{
+    let result = "";
 
+    if(num >= 0)
+    {
+        for(let x = 0; x < num; x++)
+        {   
+            result += string;
+        }
+    } else
+    {
+        result = "ERROR";
+    }
+
+    return result;
 };
 
 // Do not edit below this line
