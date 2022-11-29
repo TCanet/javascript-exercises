e12e4b6e10adb4097e72782735260f2019f7ff2b
Titus Canet <tcanet94@gmail.com>
2022-11-28 16:42:31 -0800

    Added logic to removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function() 
+{
+    let array = arguments[0];
 
+    for(let n = 1; n < arguments.length; n++)
+    {
+        for(let x = 0; x < array.length; x++)
+        {
+            if(arguments[n] === array[x] )
+            {
+                array.splice(x,1);
+            }
+        }
+    }
+
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
