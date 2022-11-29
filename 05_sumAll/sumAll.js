13004092f456fa6f8304c6b4c62827d25efa860a
Titus Canet <tcanet94@gmail.com>
2022-11-28 17:38:13 -0800

    Added logic to the sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(x, y) 
+{
+    let result = 0, larger = 0, smaller = 0;
 
+    if(x >= y)
+    {
+        larger = x;
+        y = smaller;
+    }else
+    {
+        smaller = x;
+        larger = y;
+    }
+
+    if(smaller >= 0 && larger >= 0 && typeof(smaller) == "number" && typeof(larger) == "number" )
+    {
+        for(let n = smaller; n <= larger; n++)
+        {
+            result += n;
+        }
+    }else
+    {
+        result = "ERROR"
+    }
+
+    return result;
 };
 
 // Do not edit below this line
