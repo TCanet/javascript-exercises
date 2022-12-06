56817b090753b32069c90c1a378d22a83c1b520d
Titus Canet <tcanet94@gmail.com>
2022-12-06 11:56:02 -0800

    Added functionality to determine if string is a palindrom

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function () 
+{
+    let result = false;
+    let str = arguments[0];
+    str.replace(/[ !@#%$.,?^*]/g, '');
+    let reversed = str.split("").reverse().join("");
+    if(reversed == str)
+    {
+        result = true;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
