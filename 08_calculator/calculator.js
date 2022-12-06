0fe08918f072886c97140d9595fcd2df5a0ff40c
Titus Canet <tcanet94@gmail.com>
2022-12-06 10:52:16 -0800

    Completed functionality of all calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,14 +8,39 @@ const subtract = function(x,y)
 	return x - y;
 };
 
-const sum = function(X,Y) 
+const sum = function() 
 {
-  return x + y;
+  let result = 0;
+
+  if(Array.isArray(arguments[0]) == false)
+  {
+    for(let x = 0; x < arguments.length; x++)
+    {
+      result += Number(arguments[x]);
+    }
+  }else
+  {
+    for(let x = 0; x < arguments[0].length; x++)
+    {
+      result += Number(arguments[0][x]);
+    }
+  }
+
+  return result;
 };
 
-const multiply = function(x,y) 
+const multiply = function() 
 {
-  return x*y;
+  let result = 1;
+
+  for(let x = 0; x < arguments[0].length; x++)
+  {
+    result *= Number(arguments[0][x]);
+  }
+
+  if(arguments.length == 0) result = 0;
+
+  return result;
 };
 
 const power = function(x,y) 
