f7d79e86c4e75def7a4216774f9fcc74eff49c74
Titus Canet <tcanet94@gmail.com>
2022-12-07 11:35:49 -0800

    Added funtionality to getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) 
+{
+    let titleArray = [];
 
+    for(let x = 0; x < arr.length; x++)
+    {
+        titleArray.push(arr[x].title);
+    }
+
+    return titleArray;
 };
 
 // Do not edit below this line
