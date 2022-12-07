f5b7ea5f26a4de40f8b489c820ef334cc18046c4
Titus Canet <tcanet94@gmail.com>
2022-12-07 13:52:58 -0800

    Added funtionality to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) 
+{
+    let oldest = arr[0];
+    
+    for(let x = 0; x < arr.length; x++)
+    {
+        if((oldest.yearOfDeath - oldest.yearOfBirth) < (arr[x].yearOfDeath -arr[x].yearOfBirth)) oldest = arr[x];
+    }
+    
+    return oldest;
 };
 
 // Do not edit below this line
