ebac527f4be7e233810930526f81013a75bf309b
Titus Canet <tcanet94@gmail.com>
2022-12-06 18:16:22 -0800

    Added funcitonality to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(x) 
+{
+    let target = Number(x);
+
+    let fibArray = [1,1];
+    let result = 0;
+
+    if(target > 2)
+    {
+        for(let index = 2; index < x; index++)
+        {
+            result = fibArray[index-1] + fibArray[index-2];
+            fibArray.push(result);
+        }
+    }else if(target >= 0 && target <= 2)
+    {
+        result = fibArray[target-1];
+    }else
+    {
+        result = "OOPS"
+    }
+
+    return result;
 
 };
 
